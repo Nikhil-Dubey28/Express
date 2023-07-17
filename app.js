b8899110530144ed1f36a58dc29475778f674634
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-07-17 22:08:56 +0530

    routers and filters

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const adminRoute = require('./routes/admin')
+const shopRoute = require('./routes/shop')
+
 
 
 app.use(bodyParser.urlencoded({extended: false}))
 
 
-app.use('/add-product',(req, res, next) => {
-   
-    res.send(`<form action ="/product" method = "POST"><input type="text" name = "title" placeholder ="Title"><input type="text" name = "size" placeholder='size'><button type ="submit">Add Product</button></form>`)
+app.use('/admin',adminRoute)
+app.use(shopRoute)
+
+app.use((req,res,next) => {
+    res.status(404).send(`<h1>Page not found</h1>`)
 })
-
-app.post('/product',(req,res,next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-
-app.use('/',(req, res, next) => {
-   
-    res.send(`<h1>Hello from express</h1>`)
-})
-
 
 
 app.listen(3000,() => {
