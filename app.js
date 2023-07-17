35308e8b18f8dd42284bbcb3ed64e8bf328d5092
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-07-17 20:08:48 +0530

    body parser

--- app.js
+++ app.js
@@ -1,18 +1,31 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-app.use((req, res, next) => {
-    console.log('first middleware')
-    next()
+
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.use('/add-product',(req, res, next) => {
+   
+    res.send(`<form action ="/product" method = "POST"><input type="text" name = "title" placeholder ="Title"><input type="text" name = "size" placeholder='size'><button type ="submit">Add Product</button></form>`)
 })
 
-app.use((req, res, next) => {
-    console.log('second middleware')
+app.post('/product',(req,res,next) => {
+    console.log(req.body)
+    res.redirect('/')
+})
+
+
+app.use('/',(req, res, next) => {
+   
     res.send(`<h1>Hello from express</h1>`)
 })
 
 
 
-app.listen(3000)
+app.listen(3000,() => {
+    console.log('listening on port 3000')
+})
 
 
