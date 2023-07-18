9b61cf59d2914ea1656be181a22e3f0ff43d905a
Nikhil Dubey <justmailnikhil3@gmail.com>
2023-07-18 17:20:38 +0530

    serving static files

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 const express = require('express')
-const app = express()
+const path = require('path')
 const bodyParser = require('body-parser')
 const adminRoute = require('./routes/admin')
 const shopRoute = require('./routes/shop')
 
 
 
+const app = express()
 app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static(path.join(__dirname,'public')))
 
 
 app.use('/admin',adminRoute)
 app.use(shopRoute)
 
 app.use((req,res,next) => {
-    res.status(404).send(`<h1>Page not found</h1>`)
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'))
 })
 
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,12 +1,29 @@
 const express = require('express')
+const path = require('path')
+const rootDir = require('../util/path')
 const router = express.Router()
 
 
 
-router.get('/',(req, res, next) => {
-   
-    res.send(`<h1>Hello from express</h1>`)
+router.get('/', (req, res, next) => {
+
+    res.sendFile(path.join(rootDir, 'views', 'shop.html'))
 })
+
+
+router.get('/contactus', (req, res) => {
+    res.sendFile(path.join(rootDir, 'views', 'contact.html'))
+})
+
+router.get('/success', (req, res) => {
+    res.sendFile(path.join(rootDir, 'views', 'success.html'))
+})
+
+
+router.post('/contactus', (req, res) => {
+    res.redirect('/success')
+})
+
 
 
 module.exports = router
